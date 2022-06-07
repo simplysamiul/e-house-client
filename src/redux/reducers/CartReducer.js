f733b61dd2449b8317d69794e4e823b71d12df56
Md Samiul <simplysamiul@gmail.com>
2022-06-07 11:42:56 +0600

    added product add list

--- src/redux/reducers/CartReducer.js
+++ src/redux/reducers/CartReducer.js
@@ -1,8 +1,17 @@
 export default function cartReducer (state = [], action){
     switch (action.type) {
         case "ADD_TO_CART":{
-            const newState = [...state, action.payload];
-            return newState;
+            const existProduct = state.find((product)=> product._id === action.payload._id);
+            if(existProduct){
+                const restProduct = state.filter((product) => product._id !== existProduct._id);
+                existProduct.qty = existProduct.qty + 1;
+                return [...restProduct, action.payload];
+            }
+            else{
+                const newProduct = action.payload;
+                newProduct.qty = 1;
+                return [...state, newProduct]
+            }
         }
         case "REMOVE_FROM_CART":
               return  state.filter((item)=> item._id !== action.payload);
--- src/views/custom/AddedPrtoductList.js
+++ src/views/custom/AddedPrtoductList.js
@@ -5,13 +5,17 @@ import { removeFromCart } from '../../redux/actions/cartAction';
 import '../../styles/AddedProductList.css';
 
 const AddedPrtoductList = ({addedProduct}) => {
-    const {main_img, _id} = addedProduct;
+    const {main_img, _id, product_name, price, qty} = addedProduct;
     const dispatch = useDispatch();
     return (
     <>
        <div className='added-product-list'>
             <img src={main_img} alt="" />
-            <p>Qty: 1</p>
+            <div className="add-product-price">
+            <h5>{product_name}</h5>
+            <p>$ {price}</p>
+            </div>
+            <p className='product-quantity'>Qty: {qty}</p>
             <button onClick={()=> dispatch(removeFromCart(_id))}><ImCross /></button>
         </div>
         </>
